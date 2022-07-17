e3a040d6e665499ac30e23b4d4153f8dc90ebd34
Seweryn Jagusiak <jagusiak@gmail.com>
2022-07-17 14:57:14 +0200

    Add automatyczne testy

--- automatyczne/features/kolko_i_krzyzyk.feature
+++ automatyczne/features/kolko_i_krzyzyk.feature
@@ -1,1 +1,11 @@
-Feature: Kółko i krzyżyk
+Feature: Kolko i krzyzyk
+  Scenario: Gracz zaznacza kolko
+    Given gracz uruchomil gre
+    When gracz kilka w "srodkowy" "srodkowy" kafelek
+    Then kafalek "srodkowy" "srodkowy" jest "kolkiem"
+
+  Scenario: Gracz zaznacza krzyzyk
+    Given gracz uruchomil gre
+    When gracz kilka w "srodkowy" "srodkowy" kafelek
+    When gracz kilka w "prawy" "dolny" kafelek
+    Then kafalek "prawy" "dolny" jest "krzyzykiem"
--- automatyczne/features/step_definitions/stepdef.js
+++ automatyczne/features/step_definitions/stepdef.js
@@ -3,13 +3,32 @@ const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
 const {Builder, By} = require('selenium-webdriver');
 setDefaultTimeout(600 * 1000);
 
-// Given("", () => {
-//   driver.get('https://jagusiak.github.io/xo/');
-// })
-//
-// When("", () => {
-//
-// })
-// Then(() => {
-//   assert.strictEqual(true, czyWidac);
-// })
+let driver = new Builder().forBrowser('chrome').build();
+
+const doZnaku = (tekst) => {
+  if (tekst == 'kolkiem') {
+    return 'o';
+  }
+  return 'x';
+}
+
+const kolumny = ["lewy", "srodkowy", "prawy"];
+const rzedy = ["gorny", "srodkowy", "dolny"];
+const pobierzKafelek = (kolumna, rzad) => {
+  const indeks = kolumny.indexOf(kolumna) + 3 * rzedy.indexOf(rzad);
+  return driver.findElement(By.id('s' + indeks));
+}
+
+Given('gracz uruchomil gre', function () {
+  driver.get('https://jagusiak.github.io/xo/');
+});
+
+When('gracz kilka w {string} {string} kafelek', function (kolumna, rzad) {
+  pobierzKafelek(kolumna, rzad).click();
+});
+
+Then('kafalek {string} {string} jest {string}',  async (kolumna, rzad, oczekiwanyZnak) => {
+  const element = await pobierzKafelek(kolumna, rzad);
+  const znak = await element.getAttribute('data-sign');
+  assert.strictEqual(doZnaku(oczekiwanyZnak), znak);
+});
