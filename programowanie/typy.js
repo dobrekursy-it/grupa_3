23d6aa5c169cdd7059c014edf156a76a4d86e74f
Seweryn Jagusiak <jagusiak@gmail.com>
2022-07-17 10:00:03 +0200

    Add programowanie dir

---
+++ programowanie/typy.js
@@ -0,0 +1,13 @@
+
+console.log('Witajcie'); // tekst
+console.log(1); // liczba
+console.log(3.14); // liczba
+console.log(true); // typ logiczny - prawda
+console.log(false); // fałsz
+console.log(undefined); // nie ustawiono wartosci
+let zmienna;
+console.log(zmienna); // oczekujemy undefined, jako, ze zmienna nie została ustawiona
+console.log(null); // typ pusty
+
+let zmienna2; // <- undefined
+zmienna2 = null; // ustawione, ale puste
---
+++ programowanie/zmienna.js
@@ -0,0 +1,28 @@
+// deklaracaja zmiennej
+let mojaPierwszaZmienna;
+
+console.log(mojaPierwszaZmienna);
+
+
+// dodaje wartosc do zmiennej
+mojaPierwszaZmienna = 'Witajcie';
+console.log(mojaPierwszaZmienna);
+
+mojaPierwszaZmienna = 3.14;
+console.log(mojaPierwszaZmienna);
+
+// deklaracja stalej
+const mojaPierwszaStala = 'Seweryn';
+console.log(mojaPierwszaStala);
+
+
+// deklaracaja zmiennej
+let mojaDrugaZmienna = 'Prowadzacy';
+
+// let mojaDrugaZmienna; mojaDrugaZmienna = 'Prowadzacy';
+
+console.log(mojaDrugaZmienna);
+
+
+// Błąd!
+mojaPierwszaStala = 3.14;
